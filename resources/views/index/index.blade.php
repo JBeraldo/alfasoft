aac00736278e213f964efa319db9da9ff4ac08b7
Beraldo <joao.angelo@webfloat.com.br>
2022-09-07 10:32:04 -0300

    index estática

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,18 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
-</head>
-<body>
-    <main>
-        <section>
-            <div>
-                
+@extends('layouts.site')
+    
+@section('content')
+    <div class="container">
+        <div class="list">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row list-header">
+                        <div class="col-md-1">
+                            id
+                        </div>
+                        <div class="col-md-2">
+                            nome
+                        </div>
+                        <div class="col-md-3">
+                            contato
+                        </div>
+                        <div class="col-md-3">
+                            e-mail
+                        </div>
+                        <div class="col-md-2">
+                        </div>
+                        <div class="col-md-2">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="row list-item">
+                        <div class="col-md-1">
+                            id
+                        </div>
+                        <div class="col-md-2">
+                            nome
+                        </div>
+                        <div class="col-md-2">
+                            contato
+                        </div>
+                        <div class="col-md-3">
+                            e-mail
+                        </div>
+                        <div class="col-md-2 btn btn-success">
+                            Editar
+                        </div>
+                        <div class="col-md-2 btn btn-danger">
+                            Remover
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="row list-item">
+                        <div class="col-md-1">
+                            id
+                        </div>
+                        <div class="col-md-2">
+                            nome
+                        </div>
+                        <div class="col-md-2">
+                            contato
+                        </div>
+                        <div class="col-md-3">
+                            e-mail
+                        </div>
+                        <div class="col-md-2 btn btn-success">
+                            Editar
+                        </div>
+                        <div class="col-md-2 btn btn-danger">
+                            Remover
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </section>
-    </main>
-</body>
-</html>
+        </div>
+    </div>
+@endsection
